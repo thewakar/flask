6ad904be83aaaed85f46d34e3c18a8b07c05254a
thewakar <thewakar1@gmail.com>
2022-10-02 13:58:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import os
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'containers-us-west-20.railway.app'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'A7'
+app.config['MYSQL_DB'] = 'railway'
+app.config['MYSQL_PORT'] = '6348'
+ 
+mysql = MySQL(app)
+
 
 @app.route('/')
 def index():
